8e0793a0c515b24092455290419f0263684ee752
kartiknagar Nagar <71663745+kartiknagar333@users.noreply.github.com>
2025-05-22 19:03:13 -0500

    initialized project

---
+++ hw4-kartikkumarNagar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hw4-kartikkumarNagar
+//
+//  Created by CDMStudent on 5/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hw4-kartikkumarNagar/hw4_kartikkumarNagarApp.swift
@@ -0,0 +1,17 @@
+//
+//  hw4_kartikkumarNagarApp.swift
+//  hw4-kartikkumarNagar
+//
+//  Created by CDMStudent on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct hw4_kartikkumarNagarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
